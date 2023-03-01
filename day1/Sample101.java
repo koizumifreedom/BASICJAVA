b2ef6bd407c1ac7e156ecd7b7cd3787ffa4299ba
小泉　友 <koizumi@freedomland.jp>
2023-03-01 13:23:23 +0900

    first commit

---
+++ day1/Sample101.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class Sample101 {
+    //javaを使った最も基本的なプログラム
+    public static void main(String[] args) {
+        //自動作成されたメソッド
+        System.out.println("Hello,World");   //「Hello,World」という文字列を表示
+    }
+}
